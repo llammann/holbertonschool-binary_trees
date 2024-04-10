d7615b64ad5d2a697a09249e22dcaeaee57a6f3b
llammann <bd7n2v9tr@code.edu.az>
2024-04-10 08:56:16 +0000

    New node

---
+++ binary_trees.h
@@ -0,0 +1,7 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+#endif
